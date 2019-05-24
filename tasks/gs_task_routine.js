e8f75769122ac384d0504a9f9c92eeb0ef017f36
Nikolay <nn.tverd@ains.kz>
2019-05-24 23:24:04 +0600

    arranged gs files and events functions splitted from tasks routines

--- tasks/gs_task_routine.js
+++ tasks/gs_task_routine.js
@@ -64,66 +64,8 @@ function G_Save_Changes_In_Task_Details_On_Server( _fileId, ___openned_for_edit_
   }
   throw( "ERROR. G_Save_Changes_In_Task_Details_On_Server" )
 }
-// -----------------------------------------------------------------------------
-// ADD JOURNAL EVENT
-// -----------------------------------------------------------------------------
-function G_ADD_JOURNAL_EVENT(__massive, __date, __event, __comment, __sender ){
-  Logger.log( "G_ADD_JOURNAL_EVENT(__massive, __date, __event, __comment )" );
-  Logger.log( __massive );
-  var massive = __massive;
-  var _event = {
-    date: __date,
-    event: __event,
-    comment: __comment,
-    sender: __sender
-  }
-  massive.push( _event );
-  return massive;
-}
-// -----------------------------------------------------------------------------
-// ADD JOURNAL EVENT -- COMMENT
-// -----------------------------------------------------------------------------
-function G_Add_Note_To_Task( _fileId, _listid, _taskid, _note, _sender ){
-  Logger.log( "G_Add_Note_To_Task( _fileId, _massive, _note, _sender )" );
-  var sheet = F_Get_Sheet_By_Name( _fileId, "active" );
-  var data = sheet.getDataRange().getValues();
-  return G_ADD_JOURNAL_EVENT_AND_SAVE(_fileId, _taskid, new Date(), "Adding note", _note, _sender );
-}
-// -----------------------------------------------------------------------------
-// ADD JOURNAL EVENT AND SAVE
-// -----------------------------------------------------------------------------
-function G_ADD_JOURNAL_EVENT_AND_SAVE(_fileId, _taskid, __date, __event, __comment, __sender, __stage ){
-  var stage = "active";
-  if( __stage != null ){
-    stage = __stage;
-  }
-  var sheet = F_Get_Sheet_By_Name( _fileId, stage );
-  var data = sheet.getDataRange().getValues();
-  var new_event = {
-    date: __date,
-    event: __event,
-    comment: __comment,
-    sender: __sender
-  };
-  var _events = [];
-  for( var i=0;i<data.length;i++ ){
-    if( data[i][0] == _taskid ){
-      Logger.log( "G_ADD_JOURNAL_EVENT_AND_SAVE" );
-      Logger.log( data[i][10] );
-      
-      if(data[i][10] == null || data[i][10] == "" ){
-        _events = [];
-      }
-      else _events = JSON.parse( data[i][10] );
-      _events.push( new_event );
-      
-      Logger.log( _events );
-      sheet.getRange( i+1, 11 ).setValue( JSON.stringify( _events) );
-      return G_UpdateTasksList( _fileId )
-    }
-  }
-  throw( "ERROR. G_ADD_JOURNAL_EVENT_AND_SAVE" )
-}
+
+
 // *************************************************************************
 // Moves active task on stage of control
 // **************************************************************************
